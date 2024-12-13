ea5ee0161f371e800ba2705fecb2b622f330afcf
Boffez <132230317+Boffez@users.noreply.github.com>
2024-12-13 13:44:25 +0100

    Update 3-3-power.py

--- 3-3-power.py
+++ 3-3-power.py
@@ -1,15 +1,9 @@
-import numpy as np
+# Define the array
+array = [8, 2, 5, 1, 9]
 
+# Compute the second power (square) of each element
+squared_array = [x**2 for x in array]
 
-arr = [8, 2, 5, 1, 9]
-np_arr = np.array(arr)
-
-
-def main():
-    print(f'Array: {" ".join(map(str, arr))}')
-    powers = np.power(np_arr, 2)
-    print(f'2nd power: {" ".join(map(str, powers))}')
-    
-
-if __name__ == '__main__':
-    main()
+# Print the original array and the squared values
+print("Array:", " ".join(map(str, array)))
+print("2nd power:", " ".join(map(str, squared_array)))
